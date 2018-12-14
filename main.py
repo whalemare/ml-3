e687c9cb9433dc14ac775c93f23aafad8b43487d
whalemare <nanostet@gmail.com>
2018-12-14 19:57:42 +0700

    Save commi

--- main.py
+++ main.py
@@ -1,18 +1,74 @@
+# coding=utf-8
 import split_icons as splitter
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+
 import os
 
-def getTraintFileName(index):
+variant = 10 + (11)
+numbers = [0, 2, 4]
+layers = range(3, 6)
+# activations = ["tanh", "relu"]
+activations = ['tanh', 'relu', 'identity', 'logistic']
+solvers = ['adam', 'lbfgs', 'sgd']
+
+
+def getTrainFileName(index):
     return "./rgr/data/MNIST/train/mnist_train{}.jpg".format(index)
+
 
 def getTestFileName(index):
     return "./rgr/data/MNIST/test/mnist_test{}.jpg".format(index)
+
+
+np.random.seed(21296)
+
+"""
+Задача классификации
+У нас имеется 10 классов (10 цифр)
+Каждое изображение содержит цвет от 0 до 255, имеет размер 28х28
+
+Нейронная сеть имеет 2 уровня: входной и выходной
+В качестве входных сигналов используются значения интенсивности цветовых сигналов (28*28=784)
+Входной слой будет иметь 800 нейронов
+Выходной слой будет 10 нейронов
+Каждый нейрон соответствует классу цифр (от 0 до 9)
+Выходное значение нейрона соответствует вероятности, что на картинке представлена та или иная цифра
+"""
+
 
 def main():
     # trainX, trainY, testX, testY = data()
     # print "Shapes: ", trainX.shape, trainY.shape, testX.shape, testY.shape
 
-    splitter.use(getTraintFileName(0), "./rgr/data/form/", 28, 28)
+    xTrain = np.array([])
+    yTrain = np.array([])
+    yTest = np.array([])
+    for number in numbers:
+        print "Generate for number=", number
+        filesTrain = splitter.split(getTrainFileName(number),
+                                    "./rgr/data/split/train/{}".format(number),
+                                    width=28, height=28)
+        filesTest = splitter.split(getTestFileName(number),
+                                   "./rgr/data/split/test/{}".format(number),
+                                   width=28, height=28)
 
+        xTrain.put(number, filesTrain)
+        yTest.put(number, filesTest)
+        yTrain.put(number, number)
+
+    classifier = MLPClassifier(
+        activation=activations[0],
+        solver=solvers[0],
+        hidden_layer_sizes=[3, 6],
+        random_state=1
+    )
+    classifier.fit(xTrain, yTrain)
+    result = classifier.predict(yTest)
+
+    print metrics.accuracy_score(yTest, result)
     return
 
 
--- split_icons.py
+++ split_icons.py
@@ -4,13 +4,20 @@ import glob
 from PIL import Image
 
 
-def use(src_img, dst_path, width, height):
-    if not os.path.exists(dst_path) or not os.path.isfile(src_img):
-        print 'Not exists', dst_path, src_img
+def split(source, destination, width, height):
+    if os.path.exists(destination):
+        if os.listdir(destination) > 0:
+            print destination, " already cropped"
+            return os.listdir(destination)
+    else:
+        os.makedirs(destination)
+
+    if not os.path.isfile(source):
+        print 'This is not file: ', source
         sys.exit(1)
 
     w, h = int(width), int(height)
-    im = Image.open(src_img)
+    im = Image.open(source)
     im_w, im_h = im.size
     print 'Image width:%d height:%d  will split into (%d %d) ' % (im_w, im_h, w, h)
     w_num, h_num = int(im_w / w), int(im_h / h)
@@ -21,5 +28,6 @@ def use(src_img, dst_path, width, height):
             piece = im.crop(box)
             tmp_img = Image.new('L', (w, h), 255)
             tmp_img.paste(piece)
-            img_path = os.path.join(dst_path, "%d_%d.png" % (wi, hi))
+            img_path = os.path.join(destination, "%d_%d.png" % (wi, hi))
             tmp_img.save(img_path)
+    return os.listdir(destination)
